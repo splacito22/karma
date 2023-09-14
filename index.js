5e6c1f98447313f95c7d9b48f8a6355af11a40ac
Stephany <136421961+splacito22@users.noreply.github.com>
2023-09-13 21:47:05 -0700

    installed right inquirer version

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-// Inquirer (node package manager) import
 const inquirer = require("inquirer");
 
-// File system module (node package manager) import
 const fs = require("fs");
 
-// Importing classes from ./lib/shapes directory
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
-// Function writes the SVG file using user answers from inquirer prompts
 function writeToFile(fileName, answers) {
   // File starts as an empty string
   let svgString = "";
   // Sets width and height of logo container
   svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
- 
+
   svgString += "<g>";
- 
+
   svgString += `${answers.shape}`;
 
-  
   let shapeChoice;
   if (answers.shape === "Triangle") {
     shapeChoice = new Triangle();
@@ -38,52 +33,47 @@ function writeToFile(fileName, answers) {
   // Closing </svg> tag
   svgString += "</svg>";
 
-  
   fs.writeFile(fileName, svgString, (err) => {
     err ? console.log(err) : console.log("Generated logo.svg");
   });
 }
 
-
-  inquirer
-    .prompt([
-      // Text prompt
-      {
-        type: "input",
-        message:
-          "What text would you like you logo to display? (Enter up to three characters)",
-        name: "text",
-      },
-      // Text color prompt
-      {
-        type: "input",
-        message:
-          "Choose text color (Enter color keyword OR a hexadecimal number)",
-        name: "textColor",
-      },
-      // Shape choice prompt
-      {
-        type: "list",
-        message: "What shape would you like the logo to render?",
-        choices: ["Triangle", "Square", "Circle"],
-        name: "shape",
-      },
-      // Shape color prompt
-      {
-        type: "input",
-        message:
-          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
-        name: "shapeBackgroundColor",
-      },
-    ])
-    .then((answers) => {
-      if (answers.text.length > 3) {
-        console.log("Must enter a value of no more than 3 characters");
-        promptUser();
-      } else {
-        writeToFile("logo.svg", answers);
-      }
-    });
-}
-
-promptUser();
+inquirer
+  .prompt([
+    // Text prompt
+    {
+      type: "input",
+      message:
+        "What text would you like you logo to display? (Enter up to three characters)",
+      name: "text",
+    },
+    // Text color prompt
+    {
+      type: "input",
+      message:
+        "Choose text color (Enter color keyword OR a hexadecimal number)",
+      name: "textColor",
+    },
+    // Shape choice prompt
+    {
+      type: "list",
+      message: "What shape would you like the logo to render?",
+      choices: ["Triangle", "Square", "Circle"],
+      name: "shape",
+    },
+    // Shape color prompt
+    {
+      type: "input",
+      message:
+        "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+      name: "shapeBackgroundColor",
+    },
+  ])
+  .then((answers) => {
+    if (answers.text.length > 3) {
+      console.log("Must enter a value of no more than 3 characters");
+      promptUser();
+    } else {
+      writeToFile("logo.svg", answers);
+    }
+  });
